029365a4bb50c556cb492bfa71e1fee0969c5dbd
sunju <kimtjden@naver.com>
2019-09-25 17:47:34 +0900

    후

---
+++ src/com/sungju/n1/Client.java
@@ -0,0 +1,92 @@
+package com.sungju.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		/**
+		 * 1단계 통신
+		 */
+		Scanner sn = new Scanner(System.in);
+		ServerSocket ss = null;
+		Socket sc = null;
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+		boolean check = true;
+		try {
+			while (check) {
+				sc = new Socket("211.238.142.35", 8282);
+				// ss = new ServerSocket(8282);
+				System.out.println("전송할 메세지");
+
+				String str = null;
+				str = sn.next();
+				os = sc.getOutputStream();
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				if (str.toLowerCase().equals("q")) {
+					check = !check;
+					break;
+				}
+
+				bw.write(str + "\r\n"); // 커서를 땡기고 enter
+				bw.flush(); // 강제로 버퍼를 비움
+				System.out.println("전송완료");
+
+				// sc = ss.accept();
+
+				System.out.println("대기중");
+
+				// sc = ss.accept();
+				is = sc.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+
+				String str2 = null;
+				str2 = br.readLine();
+				if (str2.toLowerCase().equals("q")) {
+					check = !check;
+					break;
+					
+				}
+
+				System.out.println(str2);
+
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				ir.close();
+				is.close();
+				bw.close();
+				ow.close();
+				os.close();
+				sc.close();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/com/sungju/n1/Server.java
@@ -0,0 +1,86 @@
+package com.sungju.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Scanner sn = null;
+		ServerSocket ss = null;
+		Socket sc = null;
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+		boolean check = true;
+
+		try {
+			ss = new ServerSocket(8282);
+			while (check) {
+
+				sn = new Scanner(System.in);
+				System.out.println("대기중");
+
+				sc = ss.accept();
+				is = sc.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+
+				String str2 = null;
+				str2 = br.readLine();
+				if (str2.toLowerCase().equals("q")) {
+					check = !check;
+					break;
+				}
+
+				System.out.println(str2);
+
+				// sc = new Socket("211.238.142.35", 8282);
+				os = sc.getOutputStream();
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				System.out.println("전송할 메세지");
+				String str = null;
+				str = sn.next();
+				if (str.toLowerCase().equals("q")) {
+					check = !check;
+					break;
+				}
+				bw.write(str + "\r\n"); // 커서를 땡기고 enter
+				bw.flush(); // 강제로 버퍼를 비움
+				System.out.println("전송완료");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bw.close();
+				ow.close();
+				os.close();
+				br.close();
+				ir.close();
+				is.close();
+				sc.close();
+				ss.close();
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
